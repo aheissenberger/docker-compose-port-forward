46ad52cab7b0da9fe72421fb45054d4dcfb2f3c1
Andreas Heissenberger <andreas@heissenberger.at>
2022-10-08 15:54:51 +0200

    overlapping ports

--- service_a/index.js
+++ service_a/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
 const request = require('request')
-const port = 3001
+const port = 3000
 
 app.get('/', (req, res) => {
   var response = 'Service A'
 
-  request('http://localhost:3002/', { json: true }, (err, res, body) => {
-    response = response + body
-  })
+  // request('http://localhost:3002/', { json: true }, (err, res, body) => {
+  //   response = response + body
+  // })
   res.send(response)  
 })
 
--- service_b/index.js
+++ service_b/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
 const request = require('request')
-const port = 3002
+const port = 3000
 
 app.get('/', (req, res) => {
   var response = 'Service B'
 
-  request('http://localhost:3001/', { json: true }, (err, res, body) => {
-    response = response + body
-  })
+  // request('http://localhost:3001/', { json: true }, (err, res, body) => {
+  //   response = response + body
+  // })
   res.send(response)  
 })
 
